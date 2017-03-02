d494ae638d1b6cb684f3ecdf8ae11b17025c735e
Nirmal Patel <nirmaljpatel@gmail.com>
2017-03-02 17:01:13 -0600

    tasks to run app in simulator & device

--- tasks/options/shell.js
+++ tasks/options/shell.js
@@ -17,6 +17,27 @@ module.exports = {
 	'appc-dist' : {
 		command : 'appc run --force -p ios -T dist-adhoc --log-level warn -R "<%= devcert %>" -P "<%= ppuid %>" -O ./dist/<%= pkg.version %>/<%= opfolder %>'
 	},
+	'appc-run-sim' : {
+		/*
+		 *  Replace with your simulator ID obtained using following command:
+		 *  $ xcrun simctl list
+		 */
+		command : 'appc run -p ios --log-level info --retina -C A7BDE792-2DA3-4B3E-807B-505F4212AE96'
+	},
+	'appc-run-trace' : {
+		/*
+		 *  Replace with your simulator ID obtained using following command:
+		 *  $ xcrun simctl list
+		 */
+		command : 'appc run -p ios --log-level trace --retina -C A7BDE792-2DA3-4B3E-807B-505F4212AE96'
+	},
+	'appc-run-device' : {
+		/*
+		 *  Replace with your Device ID obtained using following command:
+		 *  $ instruments -s devices
+		 */
+		command : 'appc run -p ios -T device --log-level info -V "Developer Certificate" -P "PPUID" --skip-js-minify -C 8bd722c4e785d3dae9a064063eaee1adc8114d7d'
+	},
 	'crittercism-create-dSYM' : { 
 		options : { 
 			stdout:true
